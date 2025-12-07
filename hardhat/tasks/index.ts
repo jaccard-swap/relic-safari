66da294eaf17cb97bcdb798ead7066beb26d683a
Taylor Hulsmans <thulsmans.1133@gmail.com>
2025-12-07 13:28:34 -0700

    factor out hh

--- hardhat/tasks/index.ts
+++
@@ -1,9 +0,0 @@
-import { task } from "hardhat/config";
-
-export const deployAuctionTask = task("deploy-auction", "Deploys the auction contract.")
-    .addFlag({name: "verify", description: "Verifies the contract on the block explorer."})
-    .addFlag({name: "noCompile", description: "Does not compile the contract."})
-    .addFlag({name: "testTokens", description: "Deploys with Test Tokens"})
-    .addFlag({name: "saveDeployments", description: "Writes deployment artifacts to artifacts/{chainId}/"})
-    .setAction(() => import("./deploy-auction.js"))
-  .build();
