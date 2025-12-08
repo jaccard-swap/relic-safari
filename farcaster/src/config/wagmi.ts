5935002373f07cb73806c6d49852ffb795ff5368
Taylor Hulsmans <thulsmans.1133@gmail.com>
2025-12-08 08:58:35 -0700

    base dep

--- farcaster/src/config/wagmi.ts
+++ farcaster/src/config/wagmi.ts
@@ -1,19 +1,21 @@
 import { http, fallback, createConfig } from 'wagmi'
 import { injected } from 'wagmi/connectors'
-import { sepolia, baseSepolia } from 'wagmi/chains'
+import { baseSepolia, base } from 'wagmi/chains'
 import {unstable_connector} from '@wagmi/core'
 import { farcasterMiniApp as miniAppConnector } from '@farcaster/miniapp-wagmi-connector'
 
-const rpcUrl = import.meta.env.VITE_BASE_SEPOLIA_RPC_URL!
+const baseSepoliaRpcUrl = import.meta.env.VITE_BASE_SEPOLIA_RPC_URL!
+const baseRpcUrl = import.meta.env.VITE_BASE_RPC_URL!
 
 export const config = createConfig({
-  chains: [baseSepolia],
+  chains: [baseSepolia, base],
   connectors: [
     injected(),
     miniAppConnector()
   ],
   transports: {
-    [baseSepolia.id]: fallback([unstable_connector(injected), http(rpcUrl)]),
+    [baseSepolia.id]: fallback([unstable_connector(injected), http(baseSepoliaRpcUrl)]),
+    [base.id]: fallback([unstable_connector(injected), http(baseRpcUrl)]),
   },
 })
 
